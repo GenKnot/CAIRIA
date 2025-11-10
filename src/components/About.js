5649c5cf3edd4c4ba294f736708b9fc849cc3826
GenKnot <167164229+GenKnot@users.noreply.github.com>
2025-11-09 19:22:26 -0500

    Remove unused Link import from About.js
    The Link component from react-router-dom was imported but not used in About.js. This commit removes the unused import to clean up the code.

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { useLanguage } from "../contexts/LanguageContext";
 import { getTranslation } from "../translations/translations";
 
